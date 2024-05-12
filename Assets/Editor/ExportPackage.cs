3f7ba84cf80fe139d2d5d1ca9db9b8308666f759
SnollyZ <90256149+SnollyZ@users.noreply.github.com>
2024-05-12 19:32:07 +0300

    ExportPackage

---
+++ Assets/Editor/ExportPackage.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEditor;
+ 
+public class ExportPackage{
+    [MenuItem ("Export/MyExport")]
+    static void export()
+    {
+        AssetDatabase.ExportPackage (AssetDatabase.GetAllAssetPaths(),PlayerSettings.productName + ".unitypackage",ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies | ExportPackageOptions.IncludeLibraryAssets);
+    }
+}
